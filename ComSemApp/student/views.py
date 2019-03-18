0ddd2b2ff08e9842c4cef559dcee600b396e8754
Daniel Richard <daniel.richard@outlook.com>
2019-03-18 14:32:24 -0700

    udapte

--- ComSemApp/student/views.py
+++ ComSemApp/student/views.py
@@ -71,7 +71,7 @@ class CourseDetailView(StudentCourseViewMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super(CourseDetailView, self).get_context_data(**kwargs)
         worksheets = self.course.worksheets.filter(status=teacher_constants.WORKSHEET_STATUS_RELEASED)
-        v bdata = super().get_context_data(**kwargs)
+
 
         
 
