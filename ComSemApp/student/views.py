ddad84ac2b5198fb9148ade03e3bafba492079f9
Daniel Richard <daniel.richard@outlook.com>
2019-03-18 20:39:01 -0700

    udapte

--- ComSemApp/student/views.py
+++ ComSemApp/student/views.py
@@ -71,7 +71,7 @@ class CourseDetailView(StudentCourseViewMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super(CourseDetailView, self).get_context_data(**kwargs)
         worksheets = self.course.worksheets.filter(status=teacher_constants.WORKSHEET_STATUS_RELEASED)
-        submissions = StudentSubmission.objects.filter(student=student)
+        submissions = StudentSubmission.objects.filter(student=self.student)
        # for submission in submissions :
          #   if submission.worksheet.course == self.course:
         print(submissions)
