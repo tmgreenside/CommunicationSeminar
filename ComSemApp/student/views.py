4f8d0faa02ff05a780d7d7aa8bbafb7272f41ffd
Daniel Richard <daniel.richard@outlook.com>
2019-03-18 20:32:52 -0700

    udapte

--- ComSemApp/student/views.py
+++ ComSemApp/student/views.py
@@ -71,7 +71,8 @@ class CourseDetailView(StudentCourseViewMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super(CourseDetailView, self).get_context_data(**kwargs)
         worksheets = self.course.worksheets.filter(status=teacher_constants.WORKSHEET_STATUS_RELEASED)
-
+        submissions = self.course.submissions.filter(student = self.student)
+        print(submissions)
 
         context['complete'] = 0
         context['incomplete'] = 0
